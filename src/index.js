f5b66ab641bea8e4f106b16bc8dfdbad06e73f02
Ilya <70850776+ShadowRoll@users.noreply.github.com>
2022-08-02 21:53:36 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const OPEN_BRACKETS = ['[', '{', '|', 'Number', '('];
+  const PALINDROM = ['|', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const BRACKET_PAIR = {
+    [')']: '(',
+    [']']: '[',
+    ['}']: '{',
+  };
+
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++){
+      let currentSymbol = str[i];
+      let topElement = stack[stack.length - 1];
+      
+      if (PALINDROM.includes(currentSymbol)){
+        if (topElement === currentSymbol){
+          stack.pop()
+        }
+        else {
+          stack.push(currentSymbol);
+        }
+      } 
+      
+      else {
+
+      if (OPEN_BRACKETS.includes(currentSymbol)){
+            stack.push(currentSymbol);
+          }
+      else {
+       
+        if (stack.length === 0){
+        return false;
+      }
+    
+      if (BRACKET_PAIR[currentSymbol] === topElement){
+        stack.pop();
+      }
+      else {
+        return false;
+      }
+    }
+  }
 }
+    return stack.length === 0;
+  }
+
